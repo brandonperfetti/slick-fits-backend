7087abd00463e50472a642530e0347e8fcb9a9a0
brandonperfeti <brandon@brandonperfetti.com>
2021-12-17 12:28:48 -0800

    Port backend from Advanced React project to ready for Digital Ocean Deploy.

---
+++ schemas/CartItem.ts
@@ -0,0 +1,26 @@
+import { list } from '@keystone-next/keystone/schema';
+import { integer, relationship, select, text } from '@keystone-next/fields';
+import { isSignedIn, rules } from '../access';
+
+export const CartItem = list({
+  access: {
+    create: isSignedIn,
+    read: rules.canOrder,
+    update: rules.canOrder,
+    delete: rules.canOrder,
+  },
+  ui: {
+    listView: {
+      initialColumns: ['product', 'quantity', 'user'],
+    },
+  },
+  // TODO - Custom Label in here
+  fields: {
+    quantity: integer({
+      defaultValue: 1,
+      isRequired: true,
+    }),
+    product: relationship({ ref: 'Product' }),
+    user: relationship({ ref: 'User.cart' }),
+  },
+});
